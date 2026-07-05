44be905bfa2048dcfcdd9bbae4161458a81de5ab
Mumtaz Azizam <azizammumtaz@gmail.com>
2026-07-05 14:52:29 +0200

    affichage de la data pour une image

--- src/gui.rs
+++ src/gui.rs
@@ -1,10 +1,11 @@
-use iced::widget::{button, column, image, row, text_input, text_editor, text, scrollable};
-use iced::{ContentFit, Element, Length, Task, Background, Color, Theme};
+use calamine::{Data, Error, RangeDeserializerBuilder, Reader, Xlsx};
+use calamine::{open_workbook, open_workbook_auto};
+use iced::widget::text_editor::Content;
+use iced::widget::{button, column, image, row, scrollable, text, text_editor, text_input};
+use iced::{Background, Color, ContentFit, Element, Length, Task, Theme};
 use std::fs;
-use std::time::SystemTime;
 use std::path::PathBuf;
-use calamine::{ open_workbook, open_workbook_auto };
-use calamine::{ Error, Xlsx, Reader, RangeDeserializerBuilder, Data};
+use std::time::SystemTime;
 
 pub struct Gui {
     image_handle: Option<image::Handle>,
@@ -16,6 +17,7 @@ pub struct Gui {
     excel_rows: Vec<Vec<String>>,
     files: Vec<(String, bool)>,
     selected_file: String,
+    current_row: Vec<String>,
 }
 
 impl Default for Gui {
@@ -29,7 +31,8 @@ impl Default for Gui {
             files: Vec::new(),
             last_modified: None,
             excel_rows: Vec::new(),
-            selected_file: "".into()
+            selected_file: "".into(),
+            current_row: Vec::new()
         }
     }
 }
@@ -64,7 +67,7 @@ impl Gui {
 
         let mut ligne = row![];
 
-        let mut fileListColumn: iced::widget::Column<'_ , Message, iced::Theme, iced::Renderer> =
+        let mut fileListColumn: iced::widget::Column<'_, Message, iced::Theme, iced::Renderer> =
             column![];
 
         for file in &self.files {
@@ -72,24 +75,24 @@ impl Gui {
                 .extension()
                 .and_then(|ext| ext.to_str())
             {
-                Some(ext) if ext.eq_ignore_ascii_case("xlsx") => Message::LoadXlsx(file.0.to_string()),
+                Some(ext) if ext.eq_ignore_ascii_case("xlsx") => {
+                    Message::LoadXlsx(file.0.to_string())
+                }
                 _ => Message::ChangePicture(file.0.to_string()),
             };
-    
+
             let current_file = self.selected_file.clone();
 
-            fileListColumn = fileListColumn.push(
-                button(text(&file.0)).on_press(message).style(move |theme: &Theme, status| {
-                            let mut style = button::text(theme, status);
-                            if current_file == file.0 {
-                                style.background = Some(Background::Color(
-                                    Color::from_rgb(0.2, 0.5, 0.9),
-                                ));
-                                style.text_color = Color::WHITE;
-                            }
-                            style
-                        }),
-            );
+            fileListColumn = fileListColumn.push(button(text(&file.0)).on_press(message).style(
+                move |theme: &Theme, status| {
+                    let mut style = button::text(theme, status);
+                    if current_file == file.0 {
+                        style.background = Some(Background::Color(Color::from_rgb(0.2, 0.5, 0.9)));
+                        style.text_color = Color::WHITE;
+                    }
+                    style
+                },
+            ));
         }
 
         ligne = ligne.push(scrollable(fileListColumn));
@@ -101,39 +104,38 @@ impl Gui {
                     .height(Length::Fill)
                     .content_fit(ContentFit::Contain),
             );
+            if(self.current_row.len()>0){
             ligne = ligne.push(
-            column![
-                field("Type :", "lettre"),
-                field("Date :", "04/01?/1892"),
-                field("Lieu :", "Clève"),
-                field("Correspondant :", "E. Lisegang"),
-                field("Note :", ""),
-                field("Langue :", "allemand"),
-                fieldBig("Contenu :", &self.content),
-                row![
-                    button("reset"),//.on_press(Message::Decrement),
-                    button("save")//.on_press(Message::Decrement)
+                column![
+                    field("Type :", &self.current_row[0]),
+                    field("Date :", &self.current_row[1]),
+                    field("Lieu :", &self.current_row[2]),
+                    field("Correspondant :", &self.current_row[3]),
+                    field("Note :", &self.current_row[4]),
+                    field("Langue :", &self.current_row[5]),
+                    fieldBig("Contenu :", &self.content),
+                    row![
+                        button("reset"), //.on_press(Message::Decrement),
+                        button("save")   //.on_press(Message::Decrement)
+                    ]
+                    .spacing(10)
                 ]
-                .spacing(10)
-            ]
-            .padding(10)
-            .spacing(8),
-        );
+                .padding(10)
+                .spacing(8),
+            );
+        }
         } else {
             let mut excel_lines: iced::widget::Column<'_, Message, iced::Theme, iced::Renderer> =
-            column![];
+                column![];
 
-             for row_data in &self.excel_rows {
-                let line = row_data.iter().fold(
-                    row![].spacing(12),
-                    |r, cell| r.push(text(cell).width(Length::Shrink))
-                );
-                    excel_lines = excel_lines.push(scrollable(line));
-
+            for row_data in &self.excel_rows {
+                let line = row_data.iter().fold(row![].spacing(12), |r, cell| {
+                    r.push(text(cell).width(Length::Shrink))
+                });
+                excel_lines = excel_lines.push(scrollable(line));
             }
 
             ligne = ligne.push(scrollable(excel_lines));
-
         }
 
         content = content.push(ligne);
@@ -154,7 +156,13 @@ impl Gui {
             Message::LoadXlsx(xlsx_path) => {
                 dbg!(&xlsx_path);
                 self.image_handle = None;
-                self.selected_file = self.current_dir.clone().unwrap_or_default().join(&xlsx_path).to_string_lossy().to_string();
+                self.selected_file = self
+                    .current_dir
+                    .clone()
+                    .unwrap_or_default()
+                    .join(&xlsx_path)
+                    .to_string_lossy()
+                    .to_string();
 
                 match load_xlsx_rows(&self.selected_file, self.last_modified) {
                     Ok(LoadResult::Unchanged) => {
@@ -170,39 +178,40 @@ impl Gui {
                     }
                 }
 
-
                 Task::none()
             }
-            Message::ChangePicture(picture_path) => {
 
+            Message::ChangePicture(picture_path) => {
                 dbg!(&picture_path);
 
-match Self::get_row_for_file(&self.files, &self.excel_rows, &picture_path) {
-    Ok(row) => {
-        println!("Ligne trouvée: {row:?}");
-    }
-    Err(e) => {
-        eprintln!("Erreur: {e}");
-    }
-}
+                match Self::get_row_for_file(&self.files, &self.excel_rows, &picture_path) {
+                    Ok(row) => {
+                        println!("Ligne trouvée: {row:?}");
+                        self.current_row = row.clone();
+                        self.content = Content::with_text(&row[6]);
+                    }
+                    Err(e) => {
+                        eprintln!("Erreur: {e}");
+                    }
+                }
 
                 self.image_handle = None;
                 self.selected_file = picture_path.clone();
 
-                        let dir = self.current_dir.clone();
-                        let picture_path_complete = dir.unwrap_or_default().join(picture_path).to_string_lossy().to_string();
+                let dir = self.current_dir.clone();
+                let picture_path_complete = dir
+                    .unwrap_or_default()
+                    .join(picture_path)
+                    .to_string_lossy()
+                    .to_string();
 
                 let path_medium = picture_path_complete.clone();
-                    let path_full = picture_path_complete.clone();
+                let path_full = picture_path_complete.clone();
 
                 self.load_generation += 1;
                 let generation = self.load_generation;
 
-
-
-
-                    Task::batch([
-                        
+                Task::batch([
                     Task::perform(
                         async move { (generation, medium_quality(&path_medium)) },
                         |(g, h)| Message::ImageStageLoaded(g, h),
@@ -220,7 +229,7 @@ match Self::get_row_for_file(&self.files, &self.excel_rows, &picture_path) {
                         self.current_dir_path = dir.to_string_lossy().to_string();
                         self.files = get_files(&self.current_dir.as_ref().unwrap());
                         dbg!(&self.current_dir_path);
-                        return Task::done(Message::LoadPressed);//Message::LoadPressed;
+                        return Task::done(Message::LoadPressed); //Message::LoadPressed;
                     }
                     None => {
                         self.current_dir = std::env::home_dir();
@@ -232,7 +241,7 @@ match Self::get_row_for_file(&self.files, &self.excel_rows, &picture_path) {
             Message::LoadPressed => {
                 self.load_generation += 1;
                 let generation = self.load_generation;
-                
+
                 if let Some(current_dir) = &self.current_dir {
                     let picture_path = self
                         .files
@@ -266,15 +275,15 @@ match Self::get_row_for_file(&self.files, &self.excel_rows, &picture_path) {
                     let path_full = picture_path.clone();
 
                     Task::batch([
-                    Task::perform(
-                        async move { (generation, medium_quality(&path_medium)) },
-                        |(g, h)| Message::ImageStageLoaded(g, h),
-                    ),
-                    Task::perform(
-                        async move { (generation, full_quality(&path_full)) },
-                        |(g, h)| Message::ImageStageLoaded(g, h),
-                    ),
-                ])
+                        Task::perform(
+                            async move { (generation, medium_quality(&path_medium)) },
+                            |(g, h)| Message::ImageStageLoaded(g, h),
+                        ),
+                        Task::perform(
+                            async move { (generation, full_quality(&path_full)) },
+                            |(g, h)| Message::ImageStageLoaded(g, h),
+                        ),
+                    ])
                 } else {
                     Task::none()
                 }
@@ -287,7 +296,12 @@ match Self::get_row_for_file(&self.files, &self.excel_rows, &picture_path) {
             }
         }
     }
-    pub fn get_row_for_file<'a>(files: &[(String, bool)], excel_rows: &'a [Vec<String>], filename: &str) -> Result<&'a Vec<String>, String> {
+
+    pub fn get_row_for_file<'a>(
+        files: &[(String, bool)],
+        excel_rows: &'a [Vec<String>],
+        filename: &str,
+    ) -> Result<&'a Vec<String>, String> {
         let index = files
             .iter()
             .position(|(name, _)| name == filename)
@@ -365,40 +379,21 @@ fn get_files(path: &PathBuf) -> Vec<(String, bool)> {
 fn field<'a>(label: &'a str, value: &'a str) -> Element<'a, Message> {
     column![
         text(label).width(150),
-        text_input("", value)
-            .width(Length::Fill),
+        text_input("", value).width(Length::Fill),
     ]
     .spacing(5)
     .into()
 }
+
 fn fieldBig<'a>(label: &'a str, value: &'a text_editor::Content) -> Element<'a, Message> {
     column![
         text(label).width(150),
         text_editor(value)
-    .size(16)
-    .height(Length::Fixed(6.0 * 22.0)),
+            .size(16)
+            .height(Length::Fixed(6.0 * 22.0)),
     ]
     .spacing(5)
     .into()
-}
-
-fn example() -> Result<(), Error> {
-    let path = format!("{}/tests/temperature.xlsx", env!("CARGO_MANIFEST_DIR"));
-    let mut workbook: Xlsx<_> = open_workbook(path)?;
-    let range = workbook.worksheet_range("Sheet1")?;
-
-
-    let mut iter = RangeDeserializerBuilder::new().from_range(&range)?;
-
-    if let Some(result) = iter.next() {
-        let (label, value): (String, f64) = result?;
-        assert_eq!(label, "celsius");
-        assert_eq!(value, 22.2222);
-        Ok(())
-    } else {
-        Err(From::from("expected at least one record but got none"))
-    }
-
 }
 
 fn load_xlsx_rows(path: &str, last_modified: Option<SystemTime>) -> Result<LoadResult, String> {
